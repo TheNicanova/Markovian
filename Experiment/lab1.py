73b9648936ca4ddd2d79b7250d69357df9d007e3
nicolas <notify.nicolas@gmail.com>
2022-06-08 15:40:01 -0400

    remove old experiments

--- Experiment/lab1.py
+++ Experiment/lab1.py
@@ -1,13 +1,14 @@
 import OptimalStopping as ops
 import OptimalStopping.BenchMark as benchmark
-import OptimalStopping.Plot as my_plot
+
 
 result = benchmark.price(
     underlying_generator=ops.UnderlyingModels.GeometricBrownianMotion(),
     option=ops.Options.Put(0.95),
     model_list=[
         ops.PricingModels.Basic(),
-        ops.PricingModels.LongStaff()
+        ops.PricingModels.LongStaff(),
+        ops.PricingModels.Rasmussen()
     ],
     n_list=[10, 20],
     m=10
